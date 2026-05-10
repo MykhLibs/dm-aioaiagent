fa63849466279075d7f8b3bb1dcd5d59067a3738
Dmytro Mykhailenko <mismartconfig@gmail.com>
2026-05-10 09:13:03 +0200

    feat(multimodal): export InputImage and OutputImage from package root

--- dm_aioaiagent/__init__.py
+++ dm_aioaiagent/__init__.py
@@ -1,3 +1,5 @@
 from .ai_agent import DMAIAgent
 from .async_ai_agent import DMAioAIAgent
+from .input_image import InputImage
+from .output_image import OutputImage
 from .openai_image_message_content import OpenAIImageMessageContent
