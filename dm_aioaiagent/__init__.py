215d5ed8f71953ea3e8fa2f858a106d27bcc11e9
Dmytro Mykhailenko <mismartconfig@gmail.com>
2026-05-10 10:26:51 +0200

    chore(multimodal)!: remove OpenAIImageMessageContent and ImageMessage TypedDict
    BREAKING CHANGE: OpenAIImageMessageContent removed; use InputImage instead.
    ImageMessage / ImageMessageTextItem / ImageMessageImageItem TypedDicts
    removed; pass HumanMessage with v1 standard content blocks (or use the
    InputImage helper) instead.

--- dm_aioaiagent/__init__.py
+++ dm_aioaiagent/__init__.py
@@ -2,4 +2,3 @@ from .ai_agent import DMAIAgent
 from .async_ai_agent import DMAioAIAgent
 from .input_image import InputImage
 from .output_image import OutputImage
-from .openai_image_message_content import OpenAIImageMessageContent
